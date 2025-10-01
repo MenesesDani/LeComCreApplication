8be82e728f60d96f4e4c8376b2abf04549a19c5b
MenesesDani <danielmeneses@alu.ufc.br>
2025-10-01 11:18:38 -0300

    Correction ButtonNext position

--- screens/HomeScreen.tsx
+++ screens/HomeScreen.tsx
@@ -1,21 +1,21 @@
 import React from "react";
 import { View, Text, StyleSheet } from "react-native";
 import CardBeta from "../components/CardBeta";
-import ButtonNext from '../components/ButtonNext';
+import ButtonNext from "../components/ButtonNext";
 
 const HomeScreen = () => {
   const handleNext = () => {
-    console.log('Botão Next pressionado!');
-    // Adicione aqui a navegação ou ação desejada
-    // navigation.navigate('ProximaTela');
+    console.log("Botão Next pressionado!");
   };
 
   return (
     <View style={styles.container}>
       <CardBeta image={require("../assets/CardRoboSmars.png")} />
+
+      {/* aqui você controla onde o ButtonNext vai ficar */}
+      <ButtonNext onPress={handleNext} style={styles.arrow} />
+
       <Text style={styles.text}>Tela Inicial</Text>
-      
-      <ButtonNext onPress={handleNext} />
     </View>
   );
 };
@@ -23,12 +23,17 @@ const HomeScreen = () => {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    alignItems: "center", // centraliza horizontalmente
-    marginTop: 20,        // distância do topo
+    alignItems: "center", // centraliza o CardBeta e o Texto
+    marginTop: 20,
+  },
+  arrow: {
+    alignSelf: "flex-end", // joga o botão para a esquerda
+    marginHorizontal: 40,          // distância da borda esquerda
+    marginTop: -24,           // espaço em relação ao CardBeta
   },
   text: {
     fontSize: 24,
-    marginTop: 20,        // distância entre o card e o texto
+    marginTop: 20,
   },
 });
 
